a99f6b2c9471424311cd3b276e313d53a4fea7ea
Mikeyao <34201037+windshowers@users.noreply.github.com>
2017-12-06 21:25:01 +0800

    Create wages increse-rate-calculator.py

---
+++ wages increse-rate-calculator.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+num1=int(input('Please enter your wages last year:'))
+num2=int(input('Please enter your wages in the year:'))
+r=(num2-num1)/num1*100
+print('For this year,you have increased your wages by %.2f%%'%r)
