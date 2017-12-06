66f230375521fcc8326af86e25c905e1500e5f19
Mikeyao <34201037+windshowers@users.noreply.github.com>
2017-12-06 21:24:09 +0800

    Create search-for-elements-in-list.py

---
+++ search-for-elements-in-list.py
@@ -0,0 +1,11 @@
+-*- coding:utf-8 -*-
+L=[['Apple','Google','Microsoft'],
+['Java','Python','Ruby','PHP'],
+['wind','blame','frost']
+]
+#print Apple:
+print(L[0][0])
+#print Python:
+print(L[1][1])
+#print frost:
+print(L[-1][-1])
