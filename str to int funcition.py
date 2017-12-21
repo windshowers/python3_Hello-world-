701c35153750b88f1f39b2ece8fb263edd0c98fe
Mikeyao <34201037+windshowers@users.noreply.github.com>
2017-12-21 21:15:31 +0800

    Create str to int funcition.py

---
+++ str to int funcition.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from functools import reduce
+DIGITS = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+def char2num(s):
+  return DIGITS[s]
+def str2int(s):
+  return reduce(lambda x,y:x*10+y,map(char2num,s))
+s=str(input('Please enter a num-str:'))
+print(str2int(s))
